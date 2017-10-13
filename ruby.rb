c56e57a22a8acd9ecf418c0ca8a5e015a27014ef
GhislainPBG <32669308+GhislainPBG@users.noreply.github.com>
2017-10-13 16:10:25 +0200

    MAJ

---
+++ ruby.rb
@@ -0,0 +1,14 @@
+# the price is right : the game!
+
+
+# computer choose a random
+
+# Ask a number to the user
+
+# If user has the right price
+# He wins
+
+# Else, computer tips the user:
+
+
+# puts "Hello"
